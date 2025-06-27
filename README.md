587d3d205243892ae022f6ac96f1b1b700637e56
yunggaketbo <yunggaketbo@gmail.com>
2025-06-27 13:12:23 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bocilartis
-bocilartis
+# [bocilartis](https://bocilartis.pages.dev)
---
+++ src/utils/site.ts
@@ -0,0 +1,4 @@
+export const nama = 'Bocil Artis';
+export const desk = 'Video bokep indo jepang viral ngentot colmek bocil tobrut ngewe jilbab smp tante.';
+export const url = 'https://bocilartis.pages.dev';
+export const terbit = '2025-06-26T00:00:00Z';
