2e3747230e4eb54f2ed801851909053cd9fcb97e
yunggaketbo <yunggaketbo@gmail.com>
2025-06-30 15:14:37 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
 export const nama = 'Bocil Artis';
-export const desk = 'Video bokep indo jepang viral ngentot colmek bocil tobrut ngewe jilbab smp tante.';
+export const desk = 'Video Bokep Indo Bocil Ngentot Abg Jilbab Artis Smp Mama Jepang Viral Sma Cindo Tante China';
 export const url = 'https://bocilartis.pages.dev';
-export const terbit = '2025-06-26T00:00:00Z';
+export const terbit = '2025-06-26T00:01:03Z';
